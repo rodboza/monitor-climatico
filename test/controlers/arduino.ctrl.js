739586f95124b1dbe2633115f445907aa0dee4df
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-09-04 11:54:13 -0300

    Update arduino.ctrl.js

--- test/controlers/arduino.ctrl.js
+++ test/controlers/arduino.ctrl.js
@@ -1,5 +1,26 @@
-class ArduinoCtrl {
+let ArduinoCtrl = {
+    
+    _app:{},
+    _chai:{},
+    _url:"",
+    
+    Setup (app, chai) {
+        _app = app;
+        _chai = chai;
+        _url = _app.GetUrl();
+    },
+    
+    GetIntervalRead (done) {
+        _chai.request(_app)
+        .get( _url + "/configuracoes/Intervalo/valor" )
+        .end((err, res) => {
+            res.should.be.status(200);
+            res.body.should.be.a('Number');
+            //res.body.should.be.eql(_configEx.valor);
+            done();
+        }
+    }
     
 }
     
-module.exports = new ArduinoCtrl();
+module.exports = ArduinoCtrl;
