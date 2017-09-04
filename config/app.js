fe4ad9bb713a6c2ccc79db24c510ffd8346350fb
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-09-04 14:12:07 -0300

    Update app.js

--- config/app.js
+++ config/app.js
@@ -1,5 +1,15 @@
 let express = require('express');
 let app = express();
+let url = "monitor-climatico";
+let consign = require('consign');
 
+
+consign({cwd: 'app'})
+  //.include('database')
+  //.then('modelo')
+  .include('api')
+  .then('rota')
+  .into(app)
+;
 
 module.exports = app;
