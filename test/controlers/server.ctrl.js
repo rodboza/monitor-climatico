9051c259ff59976e25f52f99218e5f2a3cf6adcb
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2017-09-04 13:34:01 -0300

    Update server.ctrl.js

--- test/controlers/server.ctrl.js
+++ test/controlers/server.ctrl.js
@@ -4,21 +4,21 @@ let AppCtrl =  {
     _server:{},
 
     Setup (port) {
-        _port = port;
-        _app = require("../../config/app");
+        this._port = port;
+        this._app = require("../../config/app");
     },
 
     DoBefore(done){
-        console.log(_app)
-        _server = _app.listen( _port, () => { 
-            console.log(`Inicializando o servidor na porta ${_port}.`);
+        console.log(this._app)
+        this._server = this._app.listen( this._port, () => { 
+            console.log(`Inicializando o servidor na porta ${this._port}.`);
             done(); 
         });
         
     },
     
     DoAfter (done){
-        _server.close();
+        this._server.close();
         done();
     },
     
